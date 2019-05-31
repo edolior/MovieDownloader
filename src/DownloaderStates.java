6e60dce0739cda023da7905cb53a9c300b7c71cb
edolior <edo.lior@gmail.com>
2019-05-31 16:48:50 +0300

    Init Interface

--- src/DownloaderStates.java
+++ src/DownloaderStates.java
@@ -1,2 +1,30 @@
 public interface DownloaderStates {
+
+    void turnOn();
+
+    void turnOff();
+
+    void internetOn();
+
+    void internetOff();
+
+    void fileRequest();
+
+    void downloadAborted();
+
+    void downloadError();
+
+    void errorFixed();
+
+    void movieOn();
+
+    void restartMovie();
+
+    void holdMovie();
+
+    void moveieOff();
+
+    void resume();
+
+
 }
