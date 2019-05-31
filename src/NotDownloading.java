7f84cc33a04edc82a485950f37c9c17a2596ed2b
edolior <edo.lior@gmail.com>
2019-05-31 16:53:08 +0300

    Init Classes

--- src/NotDownloading.java
+++ src/NotDownloading.java
@@ -1,2 +1,66 @@
-public class NotDownloading {
+public class NotDownloading implements DownloaderStates {
+    @Override
+    public void turnOn() {
+
+    }
+
+    @Override
+    public void turnOff() {
+
+    }
+
+    @Override
+    public void internetOn() {
+
+    }
+
+    @Override
+    public void internetOff() {
+
+    }
+
+    @Override
+    public void fileRequest() {
+
+    }
+
+    @Override
+    public void downloadAborted() {
+
+    }
+
+    @Override
+    public void downloadError() {
+
+    }
+
+    @Override
+    public void errorFixed() {
+
+    }
+
+    @Override
+    public void movieOn() {
+
+    }
+
+    @Override
+    public void restartMovie() {
+
+    }
+
+    @Override
+    public void holdMovie() {
+
+    }
+
+    @Override
+    public void moveieOff() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
 }
